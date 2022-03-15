3f17653e861e4ee3fbafc34dfb0a78367b5b6955
Maximilian Naumann <m-naumann@naumail.de>
2022-03-15 01:59:48 +0100

    Refactor a star

--- scripts/run_a_star.py
+++ scripts/run_a_star.py
@@ -1,17 +1,33 @@
-from graph_search.a_star import ExampleGraph
+import numpy as np
+
+from graph_search.a_star import Node, Graph
 
 
 def main():
-    graph = ExampleGraph()
-    graph.draw()
 
-    HH = graph.nodes[0]
-    M = graph.nodes[6]
+    nodes_list = [
+        ["HH", 170, 620, ["H", "B"]],
+        ["H", 150, 520, ["B", "L", "F", "HH"]],
+        ["B", 330, 540, ["HH", "H", "L"]],
+        ["L", 290, 420, ["B", "H", "S", "M"]],
+        ["F", 60, 270, ["H", "S"]],
+        ["S", 80, 120, ["F", "L", "M"]],
+        ["M", 220, 20, ["S", "L"]],
+    ]
+    nodes_dict = {}
+    for entry in nodes_list:
+        nodes_dict[entry[0]] = Node(
+            name=entry[0],
+            position=np.array([entry[1], entry[2]]),
+            connected_to=entry[3],
+        )
 
-    result = graph.a_star(M, HH)
-    print([x.node.name for x in result])
+    graph = Graph(nodes_dict=nodes_dict)
+    graph.draw_graph()
 
-    graph.draw_result(result)
+    graph.a_star(start="M", end="HH")
+
+    graph.draw_result()
 
 
 if __name__ == "__main__":
--- src/graph_search/a_star.py
+++ src/graph_search/a_star.py
@@ -1,159 +1,119 @@
 import matplotlib.pyplot as plt
-import math
+import numpy as np
 
 
-class GraphNode:
-    def __init__(self, name, x, y):
+class Node:
+    def __init__(self, name, position, connected_to):
         self.name = name
-        self.x = x
-        self.y = y
-        self.connected_to = []
-
-    def add_connected_to(self, connected_to):
-        self.connected_to.append(connected_to)
-
-    def distance_to(self, node):
-        return math.sqrt((self.x - node.x) ** 2 + (self.y - node.y) ** 2)
-
-
-class AStarNode:
-    def __init__(self, node, end_node):
-        self.C = 0
-        self.G = node.distance_to(end_node)
-        self.J = 0
-        self.node = node
+        self.position = position
+        self.connected_to = connected_to
         self.predecessor = None
+        self.cost_to_come = None
+        self.heuristic_cost_to_go = None
+
+    def compute_heuristic_cost_to_go(self, goal_node):
+        self.heuristic_cost_to_go = np.linalg.norm(goal_node.position - self.position)
+
+    def total_cost(self):
+        return self.cost_to_come + self.heuristic_cost_to_go
 
 
-class ExampleGraph:
-    def __init__(self):
-        HH = GraphNode("HH", 170, 620)
-        H = GraphNode("H", 150, 520)
-        B = GraphNode("B", 330, 540)
-        L = GraphNode("L", 290, 420)
-        F = GraphNode("F", 60, 270)
-        S = GraphNode("S", 80, 120)
-        M = GraphNode("M", 220, 20)
+def extract_min(node_set, node_dict):
+    min_node = min(node_set, key=lambda x: node_dict[x].total_cost())
+    node_set.remove(min_node)
+    return min_node
 
-        self.nodes = [HH, H, B, L, F, S, M]
 
-        HH.add_connected_to(H)
-        H.add_connected_to(HH)
-        HH.add_connected_to(B)
-        B.add_connected_to(HH)
+class Graph:
+    def __init__(self, nodes_dict) -> None:
+        self.nodes_dict = nodes_dict
 
-        H.add_connected_to(B)
-        B.add_connected_to(H)
-        H.add_connected_to(L)
-        L.add_connected_to(H)
-        H.add_connected_to(F)
-        F.add_connected_to(H)
-
-        B.add_connected_to(L)
-        L.add_connected_to(B)
-
-        L.add_connected_to(S)
-        S.add_connected_to(L)
-        L.add_connected_to(M)
-        M.add_connected_to(L)
-
-        F.add_connected_to(S)
-        S.add_connected_to(F)
-
-        S.add_connected_to(M)
-        M.add_connected_to(S)
+        self.end_node = None
 
         fig, ax = plt.subplots()
         self.fig = fig
         self.ax = ax
 
-    def draw(self):
+    def draw_graph(self):
         self.ax.set_xlim([0, 700])
         self.ax.set_ylim([0, 700])
 
-        for node in self.nodes:
-            self.ax.plot(node.x, node.y, marker="o", markersize=6, color="k")
-            self.ax.annotate(node.name, (node.x + 10, node.y + 10))
+        for node in self.nodes_dict.values():
+            self.ax.plot(
+                node.position[0], node.position[1], marker="o", markersize=6, color="k"
+            )
+            self.ax.annotate(node.name, (node.position[0] + 10, node.position[1] + 10))
             for connected_to in node.connected_to:
+                connected_node = self.nodes_dict[connected_to]
                 self.ax.plot(
-                    [node.x, connected_to.x],
-                    [node.y, connected_to.y],
+                    [node.position[0], connected_node.position[0]],
+                    [node.position[1], connected_node.position[1]],
                     linewidth=1,
                     color="k",
                 )
 
-    def a_star(self, start, end):
-        open_set = []
-        closed_set = []
+    def draw_result(self):
+        current_node = self.end_node
+        while self.nodes_dict[current_node].predecessor:
+            curr_node = self.nodes_dict[current_node]
+            predecessor = curr_node.predecessor
+            pred_node = self.nodes_dict[predecessor]
 
-        x_0 = AStarNode(start, end)
-
-        x_0.J = x_0.G
-
-        open_set.append(x_0)
-
-        def extract_min(open_set):
-            if open_set:
-                result = open_set[0]
-                min_J = result.J
-                for node in open_set:
-                    if node.J < min_J:
-                        result = node
-                        min_J = result.J
-
-                open_set.remove(result)
-                return result
-
-            return None
-
-        def retrace_path(node):
-            path = [node]
-
-            while node.predecessor is not None:
-                node = node.predecessor
-                path.append(node)
-
-            path.reverse()
-            return path
-
-        while open_set:
-            x = extract_min(open_set)
-            closed_set.append(x)
-
-            if x.node == end:
-                return retrace_path(x)
-            else:
-                for node in x.node.connected_to:
-                    x_tilde = AStarNode(node, end)
-
-                    if x_tilde.node not in closed_set:
-                        cost = x.C + x_tilde.node.distance_to(x.node)
-
-                        if (
-                            not x_tilde.node in [x.node for x in open_set]
-                            or cost < x_tilde.G
-                        ):
-                            x_tilde.predecessor = x
-                            x_tilde.C = cost
-                            x_tilde.J = x_tilde.C + x_tilde.G
-
-                            if not x_tilde.node in [x.node for x in open_set]:
-                                open_set.append(x_tilde)
-
-    def draw_result(self, result):
-        for i in range(len(result) - 1):
-            node_from = result[i].node
-            node_to = result[i + 1].node
             self.ax.plot(
-                [node_from.x, node_to.x],
-                [node_from.y, node_to.y],
+                [curr_node.position[0], pred_node.position[0]],
+                [curr_node.position[1], pred_node.position[1]],
                 linewidth=2,
                 color="b",
             )
 
-            distance = node_from.distance_to(node_to)
-            x_mid = (node_from.x + node_to.x) / 2
-            y_mid = (node_from.y + node_to.y) / 2
+            distance = np.linalg.norm(curr_node.position - pred_node.position)
+            x_mid = (curr_node.position[0] + pred_node.position[0]) / 2.0
+            y_mid = (curr_node.position[1] + pred_node.position[1]) / 2.0
             self.ax.annotate(f"{distance:.2f}", (x_mid + 10, y_mid + 10))
+            current_node = predecessor
 
         plt.show()
+
+    def a_star(self, start, end):
+        """
+        returns False if unsuccessful
+        """
+        self.end_node = end
+        open_set = set()
+        closed_set = set()
+        for node in self.nodes_dict.values():
+            node.compute_heuristic_cost_to_go(self.nodes_dict[end])
+
+        open_set.add(start)
+        self.nodes_dict[start].cost_to_come = 0
+
+        while open_set:
+            current_node = extract_min(node_set=open_set, node_dict=self.nodes_dict)
+
+            if current_node == end:
+                return True
+
+            closed_set.add(current_node)
+
+            for successor_node in self.nodes_dict[current_node].connected_to:
+                if successor_node in closed_set:
+                    continue
+
+                tentative_cost_to_come = self.nodes_dict[
+                    current_node
+                ].cost_to_come + np.linalg.norm(
+                    self.nodes_dict[current_node].position
+                    - self.nodes_dict[successor_node].position
+                )
+                if (
+                    successor_node in open_set
+                    and tentative_cost_to_come
+                    >= self.nodes_dict[successor_node].cost_to_come
+                ):
+                    continue
+
+                self.nodes_dict[successor_node].predecessor = current_node
+                self.nodes_dict[successor_node].cost_to_come = tentative_cost_to_come
+                open_set.add(successor_node)
+
+        return False
